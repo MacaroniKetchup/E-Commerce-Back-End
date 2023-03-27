5a8dcc6f737ead3e5b0d73ce28ea608500335d4f
MacKetchup <stylesthestyer@gmail.com>
2023-03-26 19:20:31 -0700

    updated connection.js, set up for api routes in api folder

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,27 @@
+// Preferd conncetion setup
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
+
+/* Original connection setup
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -13,3 +37,4 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+*/
